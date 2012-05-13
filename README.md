e56278376bed9df3fd8099f1ca7718c9e4ffda2f
Haisheng.W.WU <freizl@gmail.com>
2012-05-13 15:21:19 +0800

    a little notes to README

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ snaplet-oauth
 =============
 
 snaplet-oauth
+
+
+snaplet
+=============
+
+- [what is snaplet]
+
+[what is snaplet]: http://snapframework.com/docs/tutorials/snaplets-tutorial
--- test/unit-test.hs
+++ test/unit-test.hs
@@ -7,7 +7,11 @@ import Network.HTTP.Conduit
 import Control.Monad.Trans (liftIO)
 import Control.Monad.IO.Class (MonadIO)
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.Aeson (decode)
+import Test.HUnit
 
+import Network.OAuth2.OAuth2
+import Utils
 
 data Quux = Quux
 
@@ -18,3 +22,21 @@ makeLenses [''Foo]
 --appQuuxLens :: Lens Foo Quux
 --appQuuxLens = quux . snapletValue . Foo
 
+
+main :: IO ()
+main = do
+       print prop_getUid
+       print prop_getInvalidUid
+       print $ intToByteString 1234
+
+invalidUidString :: BSL.ByteString
+invalidUidString = "{\"uid\" : \"222222\" }"
+
+prop_getInvalidUid :: Maybe WeiboUserId
+prop_getInvalidUid = decode invalidUidString
+
+uidString :: BSL.ByteString
+uidString = "{\"uid\" : 222222 }"
+
+prop_getUid :: Maybe WeiboUserId
+prop_getUid = decode uidString
