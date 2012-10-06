7783724d80cd0d1712f9a5bf2f94ef1222dbdbc2
Haisheng.W.WU <freizl@gmail.com>
2012-10-06 18:54:21 +0800

    delete wrongi file

--- data/init-keys.hs
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-### run script at root dir for travis
-
-cp ./data/Key.Weibo.hs ./src/Snap/Snaplet/OAuth/Weibo/Key.hs
-cp ./data/Key.Google.hs ./src/Snap/Snaplet/OAuth/Google/Key.hs
--- data/init-keys.sh
+++ data/init-keys.sh
@@ -2,5 +2,5 @@
 
 ### run script at root dir for travis
 
-cp ./data/Key.Weibo.hs ../src/Snap/Snaplet/OAuth/Weibo/Key.hs
-cp ./data/Key.Google.hs ../src/Snap/Snaplet/OAuth/Google/Key.hs
+cp ./data/Key.Weibo.hs ./src/Snap/Snaplet/OAuth/Weibo/Key.hs
+cp ./data/Key.Google.hs ./src/Snap/Snaplet/OAuth/Google/Key.hs
